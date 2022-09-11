d7d237092dff804442c18da4f808e36b0b881d9c
urk21cs1014 <110385851+urk21cs1014@users.noreply.github.com>
2022-09-11 21:21:21 +0530

    done

--- Main.py
+++ Main.py
@@ -3,40 +3,51 @@ class Node:
     self.data = data
     self.next = None
 
-class Stack:
+
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
-    nn = Node(data)
-    nn.next = self.head
-    self.head = nn
+  def enqueue(self, data) -> None:
+    # Write your code here
+    if self.last is None:
+      self.head = Node(data)
+      self.last = self.head
+    else:
+      self.last.next = Node(data)
+      self.last = self.last.next
 
-  def pop(self) -> None:
-    if(self.head!=None):
-      temp = self.head
-      self.head = temp.next
-  def status(self):
-    if(self.head  != None):
-      temp = self.head
-      while temp.next!= None:
-        print(temp.data, end = "")
-        print("=>", end = "")
-        temp = temp.next
-      print(temp.data, end = "=>")
-      
-    print("None")
+  def dequeue(self) -> None:
+    # Write your code here
+    if self.head is None:
+      return None
+    else:
+      val_returned = self.head.data
+      self.head = self.head.next
+
+  def status(self) -> None:
+    # Write your code here
+    current = self.head
+    status_of_queue = []
+    while (current):
+      status_of_queue.append(current.data)
+      current = current.next
+    for element in status_of_queue:
+      print(element, end="=>")
+    print(None)
+
 
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
